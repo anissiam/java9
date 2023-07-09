42e7ab8582e93f598185ceee6587313f80e67127
anis <anis.siam@hotmail.com>
2023-07-09 16:00:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //This is print line
+        /*
+        This
+        is
+        print
+        multipe
+        line
+         */
+
+
+        System.out.print("ANis");
+        System.out.println("ANis");
+        int x=50;
+        System.out.println(x);
+        x = 100;
+        System.out.println(x);
+
+       /* System.out.println(5+10+50);
+        System.out.println("Hello world!");*/
+    }
+}
---
+++ src/VisionPlus.java
@@ -0,0 +1,10 @@
+public class VisionPlus {
+    public static void main(String[] args) {
+
+    }
+
+    public static void visionPlus(){
+
+    }
+}
+
