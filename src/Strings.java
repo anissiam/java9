13b30e480d15eb116d7f39badff1ae9d26704124
anissiam <anis.siam@hotmail.com>
2023-07-13 15:59:13 +0300

    - Variables -Strings -var -Operators

--- src/Strings.java
+++ src/Strings.java
@@ -1,7 +1,7 @@
 public class Strings {
     static String company ;
     public static void main(String[] args) {
-        String name = "Anis";
+        /*String name = "Anis";
         String name1 = "Anis";
         String name2 = new String("Anis");
         String lastName = "Siam";
@@ -26,5 +26,47 @@ public class Strings {
         String myName = "Anis";
         System.out.println("My name is "+ myName  + " " + lastName+ " and my age is " + age +
                 " and my last mark is " + mark + " and working on " + company);
+
+    */
+
+        //////////////////////////////////////////////////////////////////////////////////////
+
+        String line = "Hello from java development class";
+        String word = "    Anis ";
+        int wordLength = word.length();
+        int  wordLength1 =  wordLength + 5000;
+        System.out.println(line.length());
+        boolean b = line.isEmpty();
+        System.out.println(b);
+
+        System.out.println(line.concat(" ".concat(word)));
+
+
+        System.out.println(word.toUpperCase());
+        System.out.println(word.toLowerCase());
+
+
+        System.out.println(line.indexOf("a"));
+        System.out.println(line.indexOf("A"));
+        System.out.println(line.lastIndexOf("a"));
+        System.out.println(line.charAt(1));
+        String ss = line.substring(2);
+        System.out.println(line.substring(2));
+        System.out.println(line.substring(2 , 8));
+
+        line = line.replace("java", "C++");
+        System.out.println(line);
+
+        System.out.println(line.contains("java"));
+
+        System.out.println(word.trim());
+
+        System.out.println(line.startsWith("Anis"));
+        System.out.println(line.startsWith("He"));
+        System.out.println(line.endsWith("class"));
+
+
+
+
     }
 }
--- src/VisionPlus.java
+++ src/VisionPlus.java
@@ -1,13 +1,43 @@
 public class VisionPlus {
-    int g;
+
     public static void main(String[] args) {
-       // g = 50;
-        int x ;
+        int x = 10;
+        int y = 50;
+        int z = 100;
+
+        double result =( x+ y + z) /3.0;
+        System.out.println(result);
+
+        //x = x + 1;
+        x += 1;
+        x++;
+        ++x;
+
+        System.out.println(x);
+        System.out.println(x++);
+        System.out.println(++x);
+
+
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x < y);
+        System.out.println(x > y);
+        System.out.println(x == y);
+        System.out.println(x != y);
+
+        System.out.println(x);
+
+        x %= 2;
+
+        System.out.println(x % 2);
+        System.out.println(x / 2);
+
+
+
+
+
+
     }
 
-    public void visionPlus(){
-        g = 50;
-        int x ;
-    }
 }
 
